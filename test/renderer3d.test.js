dd3a578b24f0be9989ba579a1dbdfbaabe93edcb
Alex Cabello <alexander.cabello@gmail.com>
2025-09-05 07:20:45 -1000

    feat: restore game sprites in 3d renderer

--- test/renderer3d.test.js
+++ test/renderer3d.test.js
@@ -16,4 +16,10 @@ assert(mesh.geometry instanceof THREE.BoxGeometry, 'geometry');
 assert(mesh.material.color.getHex() === 0xff0000, 'color');
 assert(r.scene.children.includes(mesh), 'mesh added to scene');
 
+const sprite = { width: 10, height: 20 };
+const spriteMesh = r.addSprite('sprite', sprite, 5, 5, Math.PI / 4, 2);
+assert(spriteMesh.geometry instanceof THREE.PlaneGeometry, 'sprite geometry');
+assert(spriteMesh.material.map.image === sprite, 'sprite texture');
+assert(r.scene.children.includes(spriteMesh), 'sprite added to scene');
+
 console.log('renderer3d test passed');
