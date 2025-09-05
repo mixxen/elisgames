dacccf3a75bb69fd4c960e25138a031478f1956f
Alex Cabello <alexander.cabello@gmail.com>
2025-09-05 07:02:20 -1000

    Use CDN for three.js and adjust imports

--- test/renderer3d.test.js
+++ test/renderer3d.test.js
@@ -1,5 +1,5 @@
 import { Renderer3D } from '../renderer3d.js';
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 
 function assert(cond, msg) {
   if (!cond) throw new Error(msg);
